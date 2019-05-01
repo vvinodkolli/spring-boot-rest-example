5cfd71285046cbbb5c14b6ff5b4e322b72503c17
vvinodkolli <42858991+vvinodkolli@users.noreply.github.com>
2019-05-01 18:04:03 +0530

    fde

--- src/main/java/com/khoubyari/example/Application.java
+++ src/main/java/com/khoubyari/example/Application.java
@@ -15,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /*
  * This is the main Spring Boot application class. It configures Spring Boot, JPA, Swagger
+ * edited
  */
 
 @SpringBootApplication
